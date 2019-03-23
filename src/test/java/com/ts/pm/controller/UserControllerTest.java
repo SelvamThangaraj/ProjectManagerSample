5b4b87cf745d3757195dc7694855168580d7f5d3
26 <26@PC34.ts.com>
2019-03-23 19:29:49 +0530

    Added Junit for UserController

---
+++ src/test/java/com/ts/pm/controller/UserControllerTest.java
@@ -0,0 +1,74 @@
+package com.ts.pm.controller;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.ts.pm.ProjectManagerApplication;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ProjectManagerApplication.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UserControllerTest {
+
+	private MockMvc mockMvc;
+	
+	@Autowired
+    private WebApplicationContext wac;
+	
+	@Before
+	public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void testAllUsers() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/pm/users").accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$", hasSize(14))).andDo(print());
+
+	}
+	
+	/*@Test
+	public void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetUser() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUser() {
+		fail("Not yet implemented");
+	}*/
+		
+
+	
+
+}
