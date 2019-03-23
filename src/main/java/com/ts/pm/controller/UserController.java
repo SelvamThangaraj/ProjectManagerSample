d2772878685103a97243175564515b4802e24e3a
267692 <267692@PC340236.cts.com>
2019-03-23 16:31:21 +0530

    TDD User crud added

--- src/main/java/com/ts/pm/controller/UserController.java
+++ src/main/java/com/ts/pm/controller/UserController.java
@@ -3,6 +3,10 @@ package com.ts.pm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,44 +14,58 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ts.pm.model.User;
 import com.ts.pm.service.UserService;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/pm")
 public class UserController {
 	
 	@Autowired
 	UserService userService;
 
-	@PostMapping(path = "/add" ,consumes = "application/json", produces = "application/json")
-	public User addUser(@RequestBody User user) {
+	@PostMapping(path = "/user" ,consumes = "application/json", produces = "application/json")
+	public ResponseEntity<User> addUser(@RequestBody User user, UriComponentsBuilder builder) {
 		System.out.println("user=>"+user);
 		User addedUser=userService.saveOrUpdateUser(user);
-		return addedUser;
+		if(null==addedUser) {
+			return new ResponseEntity<User>(HttpStatus.CONFLICT);
+		}
+		HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/user/{id}").buildAndExpand(user.getUserId()).toUri());
+        return new ResponseEntity<User>(addedUser,headers, HttpStatus.CREATED);
+		
 	}
 	
-	@PutMapping(path = "/update" ,consumes = "application/json", produces = "application/json")
+	@PutMapping(path = "/user" ,consumes = "application/json", produces = "application/json")
 	public User updateUser(@RequestBody User user) {
 		System.out.println("user=>"+user);
 		User addedUser=userService.saveOrUpdateUser(user);
 		return addedUser;
 	}
 	
-	@GetMapping(path = "/{id}" ,produces = "application/json")
+	@GetMapping(path = "/user/{id}" ,produces = "application/json")
 	public User getUser(@PathVariable("id") Long id) {
 		System.out.println("id=>"+id);
 		User addedUser=userService.getUser(id);
 		return addedUser;
 	}
 	
-	@GetMapping(path = "/getAll" ,produces = "application/json")
+	@GetMapping(path = "/users" ,produces = "application/json")
 	public List<User> getAllUsers() {		
 		List<User> listUsers=userService.getAllUsers();
 		System.out.println("listUsers=>"+listUsers);
 		return listUsers;
 	}
+	
+	@DeleteMapping("/user/{id}")
+	public ResponseEntity<Long> deleteUser(@PathVariable("id") Long id) {
+		System.out.println("Delete id=>"+id);
+		Long deletedUserid=userService.deleteUser(id);
+		return new ResponseEntity<Long>(deletedUserid,HttpStatus.NO_CONTENT);
+	}	
 	
 
 }
--- src/test/java/com/ts/pm/ProjectManagerApplicationTests.java
+++ src/test/java/com/ts/pm/ProjectManagerApplicationTests.java
@@ -1,16 +1,111 @@
 package com.ts.pm;
 
+import static com.jayway.restassured.RestAssured.delete;
+import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import com.ts.pm.model.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)   
+@ContextConfiguration(classes = ProjectManagerApplication.class)  
+@TestPropertySource(value={"classpath:application.properties"})
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class ProjectManagerApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Value("${server.port}")   
+    int port;
+	
+	@Before
+	public void setBaseUri () {
+
+		 RestAssured.port = port;
+		 RestAssured.baseURI = "http://localhost"; 
 	}
+	
+		
+	@Test
+	public void postDataTest() {
+		
+		User user=new User();
+		user.setFirstName("Aaradhana");
+		user.setLastName("Selvam");
+		user.setEmployeeId("25345");
+		
+		Response response = given().
+				contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body(user)
+				.when()
+				.post("/pm/user");
+		
+		System.out.println("POST Response\n" + response.asString());
+	
+		// tests
+		response.then().body("firstName", equalTo("Aaradhana"));		
+	}
+	
+	@Test
+	public void getDataTest() {
+		//System.out.println(get("/pm/user/1").asString());
+		//get("/pm/user/1").then().assertThat().body("firstName", equalTo("Parthiban"));
+		get("/pm/user/1").then().assertThat().body("userId", equalTo(1));
+		//get("/pm/user/1").then().assertThat().body("userId", equalTo("1"));
+		//given().request().body("some body").post()		
+	}
+	
+	@Test
+	public void putDataTest() {
+		
+		User user=new User();
+		user.setUserId(5);
+		user.setFirstName("Aaradhana");
+		user.setLastName("SelvamSubathra");
+		user.setEmployeeId("25345");
+		
+		Response response = given().
+				contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body(user)
+				.when()
+				.put("/pm/user");
+		
+		System.out.println("PUT Response\n" + response.asString());
+		// tests
+		response.then().body("lastName", equalTo("SelvamSubathra"));		
+	}
+	
+	@Test
+	public void deleteDataTest() {
+		Response response = delete("/pm/user/9");
+		
+		System.out.println("DELETE Response\n" + response.asString());
+		// tests
+		response.then().statusCode(204);
+		
+	}
+	
+	@Test
+	public void listAllDataTest() {
+		Response response= get("/pm/users");
+		System.out.println("LIST Users Response\n" + response.asString());
+		response.then().body("userId", hasItems(1, 2));
+	}
+
+	
 
 }
