7979df30f7bf193154d32eebdd574bb502d8a4a0
selvamthangaraj <spark_sel@yahoo.co.in>
2019-04-01 12:19:39 +0530

    added @CrossOrigin

--- src/main/java/com/ts/pm/controller/UserController.java
+++ src/main/java/com/ts/pm/controller/UserController.java
@@ -8,13 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -22,6 +22,7 @@ import com.ts.pm.model.User;
 import com.ts.pm.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	
 	@Autowired
@@ -38,7 +39,9 @@ public class UserController {
 		}
 		HttpHeaders headers = new HttpHeaders();
         headers.setLocation(builder.path("/user/{id}").buildAndExpand(user.getUserId()).toUri());
-        return new ResponseEntity<User>(addedUser,headers, HttpStatus.CREATED);
+        ResponseEntity<User> respEntity=new ResponseEntity<User>(addedUser,headers, HttpStatus.CREATED);
+        LOGGER.debug("respEntity=>"+respEntity.toString());
+        return respEntity;
 		
 	}
 	
@@ -57,10 +60,11 @@ public class UserController {
 	}
 	
 	@GetMapping(path = "/users" ,produces = "application/json")
-	public ResponseEntity<List<User>> getAllUsers() {		
+	public List<User> getAllUsers() {		
 		List<User> listUsers=userService.getAllUsers();
 		LOGGER.debug("listUsers=>"+listUsers);
-		return new ResponseEntity<List<User>>(listUsers,HttpStatus.OK);
+		//return new ResponseEntity<List<User>>(listUsers,HttpStatus.OK);
+		return listUsers;
 	}
 	
 	@DeleteMapping("/user/{id}")
